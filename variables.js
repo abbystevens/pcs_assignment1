e15c9e9f44790d67a66ef4a8055766501956d612
abbystevens <adeats@gmail.com>
2013-10-05 23:09:38 -0700

    add js file

---
+++ variables.js
@@ -0,0 +1,11 @@
+var aValue = 10
+var bValue = 20
+var cValue = aValue + bValue
+console.log(cValue)
+var dValue = cValue/(bValue/aValue)
+console.log(dValue)
+var street = "Main Street"
+var state = "Oregon"
+var zip= "97219"
+var fullAddress = street + " " + state + " " + zip
+console.log(fullAddress)
