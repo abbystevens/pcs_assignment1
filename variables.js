eb2ca036bd419a883cbd81759b1b563d642bfa5e
David Manning <dlmanning@me.com>
2013-10-07 12:14:02 -0700

    instructor evaluation

--- variables.js
+++ variables.js
@@ -1,11 +1,25 @@
 var aValue = 10
 var bValue = 20
+
 var cValue = aValue + bValue
 console.log(cValue)
-var dValue = cValue/(bValue/aValue)
+
+var dValue = cValue / (bValue / aValue)
 console.log(dValue)
+
 var street = "Main Street"
 var state = "Oregon"
-var zip= "97219"
+var zip = "97219"
+
 var fullAddress = street + " " + state + " " + zip
 console.log(fullAddress)
+
+/*
+dlmanning comments:
+
+Well done, Abby! I've made some optional edits to help with code readability. Consider putting
+spaces between logically distinct segments of your code. Also, it's a good idea to always put
+spaces between operators and their operands.
+
+Great Work!
+*/
